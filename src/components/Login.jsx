67aa6b53301bf09adf1be576241bdef3d5389d21
AyanChikode <chikodeayan4@gmail.com>
2026-01-06 00:29:23 +0530

    axios install create perform

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,60 +1,73 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const [id, setId] = useState(undefined);
+  // Create 
+  const [data, setData] = useState({
+    name: "",
+    password: ""
+  });
+
+  const [newData, setNewData] = useState([]);
+
+  function handleChange(e) {
+    setData({ ...data, [e.target.id]: e.target.value });
+  }
+
+  function handleSubmit(e) {
     e.preventDefault();
-    console.log("Email:", email);
-    console.log("Password:", password);
-  };
+    // console.log("Form Data:", data);
+
+    axios
+      .post("https://695b986a1d8041d5eeb77c41.mockapi.io/Users", data)
+      .then((result) => {
+        // console.log("POST Success:", result.data);
+      })
+      .catch((err) => {
+        // console.log("Server Error:", err.response);
+      });
+  }
 
   return (
     <>
-
-    <div>
+      <div>
         <h1 className="text-center">Login</h1>
-    </div>
-    <div className="container d-flex justify-content-center align-items-center vh-100">
-      <div className="card shadow p-4" style={{ width: "350px" }}>
-        <h3 className="text-center mb-4 text-dark">Login</h3>
-        
-      
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label className="form-label text-dark">Email</label>
-            <input
-              className="form-control"
-              placeholder="Enter email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label className="form-label text-dark">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Enter password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-
-          {/* Button */}
-          <button type="submit" className="btn btn-dark w-100">
-            Login
-          </button>
-        </form>
       </div>
-    </div>
-    <table>
-      <td>
-        <th>
 
-        </th>
-      </td>
-    </table>
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        <div className="card shadow p-4" style={{ width: "350px" }}>
+          <h3 className="text-center mb-4 text-dark">Login</h3>
+
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label className="form-label text-dark">Name</label>
+              <input
+                id="name"
+                className="form-control"
+                placeholder="Enter Name"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label text-dark">Password</label>
+              <input
+                id="password"
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                onChange={handleChange}
+              />
+            </div>
+
+            <button type="submit" className="btn btn-dark w-100">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
